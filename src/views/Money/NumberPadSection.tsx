3636f7e905cf1d66a1d2f4ae9fb848039c19de33
hao <2524129018@qq.com>
2022-06-07 20:06:36 +0800

    重构 NumberPadSection.tsx

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,96 +1,26 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
+import { generateOutput } from './NumberPadSection/generateOutput'
+import { Wrapper } from './NumberPadSection/Wrapper'
 
-const Wrapper = styled.section`
-  > .output {
-    font-size: 36px;
-    line-height: 72px;
-    text-align: right;
-    padding-right: 6px;
-    box-shadow: inset 0px -1px 2px rgba(0, 0, 0, 0.25);
-  }
-  > .pad {
-    > button {
-      border: none;
-      width: 25%;
-      height: 64px;
-      float: left;
-      &.OK {
-        float: right;
-        height: 128px;
-      }
-      &.zero {
-        width: 50%;
-      }
-      &:nth-child(1) {
-        background: #f2f2f2;
-      }
-      &:nth-child(2),
-      &:nth-child(5) {
-        background: #e0e0e0;
-      }
-      &:nth-child(3),
-      &:nth-child(6),
-      &:nth-child(9) {
-        background: #d3d3d3;
-      }
-      &:nth-child(4),
-      &:nth-child(7),
-      &:nth-child(10) {
-        background: #c1c1c1;
-      }
-      &:nth-child(8),
-      &:nth-child(11),
-      &:nth-child(13) {
-        background: #b8b8b8;
-      }
-      &:nth-child(12) {
-        background: #9a9a9a;
-      }
-      &:nth-child(14) {
-        background: #a9a9a9;
-      }
-    }
-  }
-`
 const NumberPadSection: React.FC = () => {
-  const [output, setOutput] = useState('0')
+  const [output, _setOutput] = useState('0')
+  const setOutput = (output: string) => {
+    if (output.length > 16) {
+      output = output.slice(0, 16)
+    } else if (output.length === 0) {
+      output = '0'
+    }
+    _setOutput(output)
+  }
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent
     if (text === null) {
       return
     }
-    switch (text) {
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        if (output === '0') {
-          setOutput(text)
-        } else {
-          setOutput(output + text)
-        }
-        console.log(text)
-        break
-      case '.':
-        console.log('点')
-        break
-      case '删除':
-        console.log('删除')
-        break
-      case '清空':
-        console.log('清空')
-        break
-      case 'OK':
-        console.log('OK')
-        break
+    if (text === 'OK') {
+      return
     }
+    setOutput(generateOutput(text, output))
   }
   return (
     <Wrapper>
