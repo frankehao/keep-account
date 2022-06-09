cc90e69a727f47be98bb756cbad7ce0c691e321a
hao <2524129018@qq.com>
2022-06-09 22:52:57 +0800

    创建 /tags/:tag 路由，学会 exact 的用法

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,42 @@
-import React from 'react'
-import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-import Money from 'views/Money'
-import NoMatch from 'views/NoMatch'
-import Tags from 'views/Tags'
-import Statistics from 'views/Statistics'
-import styled from 'styled-components'
+import React from 'react';
+import {HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import Money from 'views/Money';
+import NoMatch from 'views/NoMatch';
+import Tags from 'views/Tags';
+import Statistics from 'views/Statistics';
+import styled from 'styled-components';
+import {Tag} from 'views/Tag';
 
 const AppWrapper = styled.div`
   color: #333;
-`
+`;
+
 function App() {
   return (
     <AppWrapper>
       <Router>
         <Switch>
-          <Route path="/tags">
-            <Tags />
+          <Route exact path="/tags">
+            <Tags/>
           </Route>
-          <Route path="/money">
-            <Money />
+          <Route exact path="/tags/:tag">
+            <Tag/>
           </Route>
-          <Route path="/statistics">
-            <Statistics />
+
+          <Route exact path="/money">
+            <Money/>
           </Route>
-          <Redirect exact from="/" to="/money" />
+          <Route exact path="/statistics">
+            <Statistics/>
+          </Route>
+          <Redirect exact from="/" to="/money"/>
           <Route path="*">
-            <NoMatch />
+            <NoMatch/>
           </Route>
         </Switch>
       </Router>
     </AppWrapper>
-  )
+  );
 }
 
-export default App
+export default App;
