0f6714284e0258fc272d32de907c2d7d0c670cb4
hao <2524129018@qq.com>
2022-06-11 17:51:11 +0800

    点击回退

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -8,38 +8,26 @@ const defaultTags = [
   {id: createId(), name: '行'},
 ];
 const useTags = () => {
-  const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
-  const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
-  const findTagIndex = (id: number) => {
-    let result = -1;
-    for (let i = 0; i < tags.length; i++) {
-      if (tags[i].id === id) {
-        result = i;
-        break;
+    const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
+    const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
+    const findTagIndex = (id: number) => {
+      let result = -1;
+      for (let i = 0; i < tags.length; i++) {
+        if (tags[i].id === id) {
+          result = i;
+          break;
+        }
       }
-    }
-    return result;
-  };
-  const updateTag = (id: number, obj: { name: string }) => {
-    const index = findTagIndex(id);
-    // 深拷贝 tags 得到 tagsClone
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    //数据不可以修改，但是可以替换，我们重新拷贝了一个新的，然后放了进去
-    // tagsClone 的第 index 删掉，换成 {id:id, name: obj.name}
-    tagsClone.splice(index, 1, {id: id, name: obj.name});
-    setTags(tagsClone);
-  };
-  const deleteTag = (id:number) => {
-    const index = findTagIndex(id);
-    // 深拷贝 tags 得到 tagsClone
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    //数据不可以修改，但是可以替换，我们重新拷贝了一个新的，然后放了进去
-    // tagsClone 的第 index 删掉，换成 {id:id, name: obj.name}
-    tagsClone.splice(index, 1);
-     setTags(tagsClone);
-
-  };
-  return {tags, setTags, findTag, updateTag, findTagIndex, deleteTag};
-};
+      return result;
+    };
+    const updateTag = (id: number, {name}: { name: string }) => {
+      setTags(tags.map(tag => tag.id === id ? {id, name: name} : tag));
+    };
+    const deleteTag = (id: number) => {
+      setTags((tags.filter(tag => tag.id !== id)));
+    };
+    return {tags, setTags, findTag, updateTag, findTagIndex, deleteTag};
+  }
+;
 export {useTags};
 
